65b863609e95e9fe36d7b935db3fe693de1aa79f
bbyoda <tri.ngo@stud.hn.de>
2021-05-26 13:51:07 +0200

    initial commit

--- src/main/java/com/project/docmanager/Config/DatabaseConfig.java
+++ src/main/java/com/project/docmanager/Config/DatabaseConfig.java
@@ -5,18 +5,26 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
 
     @Bean
     public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("mysql://b067401cf5ec73:ba7d3d21@eu-cdbr-west-01.cleardb.com/heroku_e516bc03f8eea12?reconnect=true");
+        dataSource.setUsername("b067401cf5ec73");
+        dataSource.setPassword("ba7d3d21");
+        return dataSource;
     }
 }
